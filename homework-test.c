b2193c7efd15a3162b8de741027a63fb3c6efdc1
wkdrkdb <gayou20065@naver.com>
2024-09-06 00:26:08 +0900

    clang

---
+++ homework-test.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+    for(int i = 0; i < 4; i++)
+    {
+
+        for(int j = 0; j < i+1; j++)
+        {
+
+            printf("*");
+
+        }
+
+
+        printf("\n");
+
+    }
+
+
+    return 0;
+}
