77090033a1ee769c64b9f009f8f3672add907f9c
ITSC <itsc@example.com>
2024-09-07 10:08:21 +0900

    hw

---
+++ homework-04.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int main(void)
+{
+    for(int i = 0; i < 7; i++)
+    {
+
+         for(int j = 0; j < 7; j++)
+        {
+           
+                if(j < 3-i)
+                {
+                    printf(" ");
+                }
+
+
+                else if (j > 3 + i)
+                {
+                    printf(" ");
+                }
+                    
+
+                else if (j < i - 3)
+                {
+                    printf(" ");
+                }
+
+                 else if (i + j >= 10)
+                {
+                    printf(" ");
+                }
+
+                else 
+                {
+                     printf("*");
+                }
+
+               
+        } 
+
+            printf("\n"); 
+               
+          
+    }
+
+           return 0;
+        
+}
+
+
+
+   
