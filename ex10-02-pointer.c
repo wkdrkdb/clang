51a271d7058cdf8210cc4d0e4bfc74ca48a46457
wkdrkdb <gayou20065@naver.com>
2024-09-19 00:11:31 +0900

    clang

---
+++ ex10-02-pointer.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int num = 10;
+    int *pnum;
+    pnum = &num;
+
+    printf("%d\n", *pnum);
+
+
+    printf("1) num의 주소값: %p\n", &num);
+    printf("2) pnum의 주소값: %p\n", pnum);
+
+
+
+    num = 14;
+    
+    printf("3) num의 주소값: %p\n", &num);
+    printf("4) pnum의 주소값: %p\n", pnum);
+
+    printf("5) pnum을 이용한 num의 값 가져오기: %d\n", *pnum);
+
+    
+    
+    *pnum = 30;
+
+    printf("6) num의 주소값: %p\n", &num);
+    printf("7) pnum의 주소값: %p\n", pnum);
+
+    printf("8) pnum을 이용한 num의 값 가져오기: %d\n", *pnum);
+
+
+    (*pnum)++;
+    (*pnum)++;
+
+    printf("9) pnum을 이용한 num의 값 가져오기: %d\n", *pnum);
+
+    
+    return 0;
+
+}
