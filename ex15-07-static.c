0a0ab019c2d13240284bd866520601bc2f00614e
wkdrkdb <gayou20065@naver.com>
2024-09-21 23:22:21 +0900

    clang

---
+++ ex15-07-static.c
@@ -0,0 +1,30 @@
+/*
+    static 변수(정적변수)
+        함수내부(지역), 함수외부(전역)에서도 사용가능
+        프로그램이 시작될 때 할당되고 프로그램이 끝날 때 소멸된다. 
+
+*/
+
+#include <stdio.h>
+
+int gNumber = 10;
+
+void increaseNumber()
+{
+    static int number = 0;  // static 전역변수 number 선언
+
+    number++;
+    printf("number: %d\n", number);
+ 
+}
+
+int main(void)
+{
+    increaseNumber();
+    increaseNumber();
+    increaseNumber();
+    increaseNumber();
+
+
+    return 0;
+}
---
+++ ex16-02-function-array.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int readArray(int arr[], int length)    
+{
+    int i;
+    printf("배열의 요소 읽어보기: [ ");
+    for(i = 0; i < length; i++)
+    {
+        printf("%d", arr[i]);
+        
+        if(i + 1 < length)
+        {
+            printf(", ");    
+        }
+
+        else
+        {
+            printf(" ");
+        }
+
+    }
+
+    printf("]\n");
+
+
+}
+
+
+int main(void)
+{
+    int arr[3] = {3, 6, 9};
+    int length = 3;
+
+    printf("arr 주소값: %p\n ", arr);   // 005FFE90
+
+    readArray(arr, length);
+
+
+    return 0;
+} 
